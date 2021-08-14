2abe52798e53e24621f4b98deaa320252ed95187
matthew <harris@ircam.fr>
2021-08-14 15:36:55 +0200

    directory structure improvements

--- code/applications/offline/MainComponent.h
+++ code/applications/offline/MainComponent.h
@@ -1,13 +1,8 @@
 #pragma once
 
-// CMake builds don't use an AppConfig.h, so it's safe to include juce module headers
-// directly. If you need to remain compatible with Projucer-generated builds, and
-// have called `juce_generate_juce_header(<thisTarget>)` in your CMakeLists.txt,
-// you could `#include <JuceHeader.h>` here instead, to make all your module headers visible.
-
 #include "JuceHeader.h"
 #include "RubberBandStretcher.h"
-#include "../RingBuffer.h"
+#include "../../core/RingBuffer.h"
 
 //==============================================================================
 class MainComponent   : public juce::AudioAppComponent, juce::ChangeListener
@@ -41,8 +36,10 @@ private:
     void stretchValueChanged();
     void pitchShiftValueChanged();
     
-    void changeState(TransportState newState);
+    void performOfflineStretch();
     
+    void changeState(TransportState newState);
+
     void changeListenerCallback (juce::ChangeBroadcaster* source) override;
     
     //==============================================================================
@@ -57,22 +54,18 @@ private:
     juce::Slider mPitchShiftSlider;
     
     juce::AudioFormatManager mFormatManager;
-    std::unique_ptr<juce::AudioFormatReaderSource> mReaderSource;
     juce::AudioTransportSource mTransportSource;
+    juce::AudioSampleBuffer mFileBuffer;
+    juce::AudioSampleBuffer mStretchedBuffer;
+    
+    std::unique_ptr<juce::MemoryAudioSource> mStretchedSrc;
+    std::unique_ptr<juce::AudioFormatReaderSource> mReaderSource;
+    std::unique_ptr<RubberBand::RubberBandStretcher> mRubberBandStretcher;
     
     TransportState mState;
     
-    std::unique_ptr<RubberBand::RubberBandStretcher> mRubberBand;
-    
-    juce::AudioSampleBuffer mInput;
-    juce::AudioSampleBuffer mScratchBuffer;
-    RubberBand::RingBuffer<float>** mOutputBuffer;
-    
     int mBlockSize;
     int mSampleRate;
-    
-    int mMinfill = 0;
-    
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
--- code/applications/real_time/MainComponent.h
+++ code/applications/real_time/MainComponent.h
@@ -1,8 +1,13 @@
 #pragma once
 
+// CMake builds don't use an AppConfig.h, so it's safe to include juce module headers
+// directly. If you need to remain compatible with Projucer-generated builds, and
+// have called `juce_generate_juce_header(<thisTarget>)` in your CMakeLists.txt,
+// you could `#include <JuceHeader.h>` here instead, to make all your module headers visible.
+
 #include "JuceHeader.h"
 #include "RubberBandStretcher.h"
-#include "../RingBuffer.h"
+#include "../../core/RingBuffer.h"
 
 //==============================================================================
 class MainComponent   : public juce::AudioAppComponent, juce::ChangeListener
@@ -36,10 +41,8 @@ private:
     void stretchValueChanged();
     void pitchShiftValueChanged();
     
-    void performOfflineStretch();
-    
     void changeState(TransportState newState);
-
+    
     void changeListenerCallback (juce::ChangeBroadcaster* source) override;
     
     //==============================================================================
@@ -54,18 +57,22 @@ private:
     juce::Slider mPitchShiftSlider;
     
     juce::AudioFormatManager mFormatManager;
-    juce::AudioTransportSource mTransportSource;
-    juce::AudioSampleBuffer mFileBuffer;
-    juce::AudioSampleBuffer mStretchedBuffer;
-    
-    std::unique_ptr<juce::MemoryAudioSource> mStretchedSrc;
     std::unique_ptr<juce::AudioFormatReaderSource> mReaderSource;
-    std::unique_ptr<RubberBand::RubberBandStretcher> mRubberBandStretcher;
+    juce::AudioTransportSource mTransportSource;
     
     TransportState mState;
     
+    std::unique_ptr<RubberBand::RubberBandStretcher> mRubberBand;
+    
+    juce::AudioSampleBuffer mInput;
+    juce::AudioSampleBuffer mScratchBuffer;
+    RubberBand::RingBuffer<float>** mOutputBuffer;
+    
     int mBlockSize;
     int mSampleRate;
+    
+    int mMinfill = 0;
+    
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
