00eca8dc5386ba12a2ea82fbbc8f71ac8f4f8755
matthew <harris@ircam.fr>
2021-08-28 12:59:51 +0200

    LookAndFeel: create custom lookAndFeel subclass

---
+++ code/ui/CustomLookAndFeel.cpp
@@ -0,0 +1,11 @@
+#include "CustomLookAndFeel.h"
+
+void CustomLookAndFeel::drawRotarySlider (juce::Graphics&,
+                                          int x, int y, int width, int height,
+                                          float sliderPosProportional,
+                                          float rotaryStartAngle,
+                                          float rotaryEndAngle,
+                                          juce::Slider&)
+{
+    
+}
---
+++ code/ui/CustomLookAndFeel.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+class CustomLookAndFeel : public juce::LookAndFeel_V4
+{
+    void drawRotarySlider (juce::Graphics&,
+                           int x, int y, int width, int height,
+                           float sliderPosProportional,
+                           float rotaryStartAngle,
+                           float rotaryEndAngle,
+                           juce::Slider&) override;
+};
