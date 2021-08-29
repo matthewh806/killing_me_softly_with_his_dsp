39d38c041a990d86e82707026e402b4ea2db7c71
matthew <harris@ircam.fr>
2021-08-29 16:36:49 +0200

    UI: Further extract MainWindow and MenuBar

--- code/applications/delay/Main.cpp
+++ code/applications/delay/Main.cpp
@@ -1,5 +1,6 @@
-#include "MainComponent.h"
+#include "../../ui/MainWindow.h"
 #include "../../ui/CustomLookAndFeel.h"
+#include "MainComponent.h"
 
 //==============================================================================
 class GuiAppApplication  : public juce::JUCEApplication
@@ -16,15 +17,9 @@ public:
     void initialise (const juce::String& commandLine) override
     {
         juce::ignoreUnused (commandLine);
-        mainWindow.reset (new MainWindow (getApplicationName(), mDefaultDeviceManager));
+        mainWindow.reset (new MainWindow (getApplicationName(), new MainComponent(mDefaultDeviceManager), mDefaultDeviceManager));
         
         mainWindow->setLookAndFeel(&customLookAndFeel);
-
-        mCommandManager.registerAllCommandsForTarget(mainWindow.get());
-        mMenuModel.setApplicationCommandManagerToWatch(&mCommandManager);
-#if JUCE_MAC && (!defined(JUCE_IOS))
-        juce::MenuBarModel::setMacMainMenu(&mMenuModel);
-#endif
     }
 
     void shutdown() override
@@ -44,160 +39,11 @@ public:
     {
         juce::ignoreUnused (commandLine);
     }
-    
-    class MainMenuModel
-    : public juce::MenuBarModel
-    {
-    public:
-        
-        MainMenuModel(juce::ApplicationCommandManager& commandManager)
-        : mCommandManager(commandManager)
-        {
-            
-        }
-        
-        ~MainMenuModel() override = default;
-        
-        juce::StringArray getMenuBarNames() override
-        {
-            return {"Settings"};
-        }
-        
-        juce::PopupMenu getMenuForIndex(int index, juce::String const& categoryName) override
-        {
-            juce::ignoreUnused(index);
-            juce::PopupMenu menu;
-            
-            auto const commandIds = mCommandManager.getCommandsInCategory(categoryName);
-            for(auto const commandId : commandIds)
-            {
-                menu.addCommandItem(&mCommandManager, commandId);
-            }
-            
-            return menu;
-        }
-        
-        void menuItemSelected(int itemId, int index) override
-        {
-            juce::ignoreUnused(itemId, index);
-        }
-        
-    private:
-        juce::ApplicationCommandManager& mCommandManager;
-        
-    };
-
-    //==============================================================================
-    /*
-        This class implements the desktop window that contains an instance of
-        our MainComponent class.
-    */
-    class MainWindow
-    : public juce::DocumentWindow
-    , public juce::ApplicationCommandTarget
-    {
-    public:
-        explicit MainWindow (juce::String name, juce::AudioDeviceManager& deviceManager)
-            : DocumentWindow (name,
-                              juce::Desktop::getInstance().getDefaultLookAndFeel()
-                                                          .findColour (ResizableWindow::backgroundColourId),
-                              DocumentWindow::allButtons)
-            , mAudioDeviceComponent(deviceManager, 0, 256, 0, 256, false, false, false, true)
-        {
-            setUsingNativeTitleBar (true);
-            setContentOwned (new MainComponent(deviceManager), true);
-
-           #if JUCE_IOS || JUCE_ANDROID
-            setFullScreen (true);
-           #else
-            setResizable (true, true);
-            centreWithSize (getWidth(), getHeight());
-           #endif
-
-            mAudioDeviceComponent.setSize(300, 300);
-            setVisible (true);
-        }
-
-        void closeButtonPressed() override
-        {
-            JUCEApplication::getInstance()->systemRequestedQuit();
-        }
-        
-        juce::ApplicationCommandTarget* getNextCommandTarget() override
-        {
-            return nullptr;
-        }
-        
-        enum CommandIds
-        {
-            AudioDeviceSettings = 0x2002000,
-        };
-
-        void getAllCommands(juce::Array<juce::CommandID>& commands) override
-        {
-            const juce::CommandID ids[] =
-            {
-                CommandIds::AudioDeviceSettings
-            };
-            
-            commands.addArray(ids, numElementsInArray(ids));
-        }
-        
-        void getCommandInfo(juce::CommandID commandID, juce::ApplicationCommandInfo& result) override
-        {
-            switch (commandID)
-            {
-                case CommandIds::AudioDeviceSettings:
-                {
-                    result.setInfo(juce::translate("Audio Device Settings"), juce::translate("Configure audio device settings"), "Settings", 0);
-                    break;
-                }
-            }
-        }
-        
-        bool perform(juce::ApplicationCommandTarget::InvocationInfo const& info) override
-        {
-            switch(info.commandID)
-            {
-                case CommandIds::AudioDeviceSettings:
-                {
-                    showDeviceSettings();
-                    return true;
-                }
-            }
-            
-            return false;
-        }
-        
-        void showDeviceSettings()
-        {
-            juce::DialogWindow::LaunchOptions o;
-            o.dialogTitle = juce::translate("Audio Device Settings");
-            
-            o.content.setNonOwned(&mAudioDeviceComponent);
-            o.componentToCentreAround = nullptr;
-            o.dialogBackgroundColour = juce::Desktop::getInstance().getDefaultLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId);
-            o.escapeKeyTriggersCloseButton = true;
-            o.useNativeTitleBar = false;
-            o.resizable = false;
-            o.useBottomRightCornerResizer = false;
-            
-            o.launchAsync();
-        }
-
-    private:
-        juce::AudioDeviceSelectorComponent mAudioDeviceComponent;
-        
-        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
-    };
-    
 
 private:
     juce::AudioDeviceManager mDefaultDeviceManager;
     
     std::unique_ptr<MainWindow> mainWindow;
-    juce::ApplicationCommandManager mCommandManager;
-    MainMenuModel mMenuModel {mCommandManager};
     CustomLookAndFeel customLookAndFeel;
 };
 
---
+++ code/ui/MainWindow.h
@@ -0,0 +1,152 @@
+#pragma once
+
+#include "JuceHeader.h"
+
+class MainMenuModel
+: public juce::MenuBarModel
+{
+public:
+    
+    MainMenuModel(juce::ApplicationCommandManager& commandManager)
+    : mCommandManager(commandManager)
+    {
+        
+    }
+    
+    ~MainMenuModel() override = default;
+    
+    juce::StringArray getMenuBarNames() override
+    {
+        return {"Settings"};
+    }
+    
+    juce::PopupMenu getMenuForIndex(int index, juce::String const& categoryName) override
+    {
+        juce::ignoreUnused(index);
+        juce::PopupMenu menu;
+        
+        auto const commandIds = mCommandManager.getCommandsInCategory(categoryName);
+        for(auto const commandId : commandIds)
+        {
+            menu.addCommandItem(&mCommandManager, commandId);
+        }
+        
+        return menu;
+    }
+    
+    void menuItemSelected(int itemId, int index) override
+    {
+        juce::ignoreUnused(itemId, index);
+    }
+    
+private:
+    juce::ApplicationCommandManager& mCommandManager;
+};
+
+class MainWindow
+: public juce::DocumentWindow
+, public juce::ApplicationCommandTarget
+{
+public:
+    explicit MainWindow (juce::String name, juce::Component* mainComponent, juce::AudioDeviceManager& deviceManager)
+        : DocumentWindow (name,
+                          juce::Desktop::getInstance().getDefaultLookAndFeel()
+                                                      .findColour (ResizableWindow::backgroundColourId),
+                          DocumentWindow::allButtons)
+        , mAudioDeviceComponent(deviceManager, 0, 256, 0, 256, false, false, false, true)
+    {
+        setUsingNativeTitleBar (true);
+        setContentOwned (mainComponent, true);
+
+       #if JUCE_IOS || JUCE_ANDROID
+        setFullScreen (true);
+       #else
+        setResizable (true, true);
+        centreWithSize (getWidth(), getHeight());
+       #endif
+        
+        mCommandManager.registerAllCommandsForTarget(this);
+        mMenuModel.setApplicationCommandManagerToWatch(&mCommandManager);
+#if JUCE_MAC && (!defined(JUCE_IOS))
+        juce::MenuBarModel::setMacMainMenu(&mMenuModel);
+#endif
+
+        mAudioDeviceComponent.setSize(300, 300);
+        setVisible (true);
+    }
+
+    void closeButtonPressed() override
+    {
+        JUCEApplication::getInstance()->systemRequestedQuit();
+    }
+    
+    juce::ApplicationCommandTarget* getNextCommandTarget() override
+    {
+        return nullptr;
+    }
+    
+    enum CommandIds
+    {
+        AudioDeviceSettings = 0x2002000,
+    };
+
+    void getAllCommands(juce::Array<juce::CommandID>& commands) override
+    {
+        const juce::CommandID ids[] =
+        {
+            CommandIds::AudioDeviceSettings
+        };
+        
+        commands.addArray(ids, numElementsInArray(ids));
+    }
+    
+    void getCommandInfo(juce::CommandID commandID, juce::ApplicationCommandInfo& result) override
+    {
+        switch (commandID)
+        {
+            case CommandIds::AudioDeviceSettings:
+            {
+                result.setInfo(juce::translate("Audio Device Settings"), juce::translate("Configure audio device settings"), "Settings", 0);
+                break;
+            }
+        }
+    }
+    
+    bool perform(juce::ApplicationCommandTarget::InvocationInfo const& info) override
+    {
+        switch(info.commandID)
+        {
+            case CommandIds::AudioDeviceSettings:
+            {
+                showDeviceSettings();
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    void showDeviceSettings()
+    {
+        juce::DialogWindow::LaunchOptions o;
+        o.dialogTitle = juce::translate("Audio Device Settings");
+        
+        o.content.setNonOwned(&mAudioDeviceComponent);
+        o.componentToCentreAround = nullptr;
+        o.dialogBackgroundColour = juce::Desktop::getInstance().getDefaultLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId);
+        o.escapeKeyTriggersCloseButton = true;
+        o.useNativeTitleBar = false;
+        o.resizable = false;
+        o.useBottomRightCornerResizer = false;
+        
+        o.launchAsync();
+    }
+
+private:
+    juce::AudioDeviceSelectorComponent mAudioDeviceComponent;
+    
+    juce::ApplicationCommandManager mCommandManager;
+    MainMenuModel mMenuModel {mCommandManager};
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
+};
