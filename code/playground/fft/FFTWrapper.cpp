19631b0b2f28f8ccc52618777ccd8d60de4a3cfe
matthewh806 <matthew.harris@ircam.fr>
2022-04-22 17:46:06 +0200

    add separate FFTWrapper class

---
+++ code/playground/fft/FFTWrapper.cpp
@@ -0,0 +1,28 @@
+#include "FFTWrapper.h"
+
+const std::vector<float>& FFTWrapper::getMagnitudes() const
+{
+    return mMagnitudes;
+}
+
+const std::vector<float>& FFTWrapper::getPhases() const
+{
+    return mPhases;
+}
+
+void FFTWrapper::performRealForwardTransform(float* data)
+{
+     mFFT.performRealOnlyForwardTransform(data);
+    
+    auto* complexOutput = reinterpret_cast<juce::dsp::Complex<float>*> (data);
+    for (size_t i = 0; i < FFTSize; ++i)
+    {
+        mMagnitudes[i] = std::abs(complexOutput[i]);
+        mPhases[i] = std::arg(complexOutput[i]);
+    }
+}
+
+void FFTWrapper::performRealInverseTransform(float* data)
+{
+    mFFT.performRealOnlyInverseTransform(data);
+}
