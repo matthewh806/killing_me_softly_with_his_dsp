19846a2eafdb5c08f945b83ec550018e35adf3f9
matthewh806 <matthew.harris@ircam.fr>
2022-04-30 18:21:04 +0200

    Add ramp envelope to FM synth

--- code/experiments/utils_functions.py
+++ code/experiments/utils_functions.py
@@ -10,3 +10,6 @@ def generateSineSignal(frequency = 220.0, length=1.0, fs=44100.0):
     samples = np.arange(length * fs) / fs
     signal = np.sin(2 * np.pi * frequency * samples)
     return signal
+
+def generateRampEnvelope(minimum_amplitude = 0.0, maximum_amplitude = 1.0, signal_length = 1.0, sample_rate = 44100.0):
+    return np.linspace(minimum_amplitude, maximum_amplitude, int(1 / signal_length * sample_rate))
