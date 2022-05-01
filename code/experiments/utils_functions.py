4bca14e15b4212f1f295cff6fff965d334cdb3d0
matthewh806 <matthew.harris@ircam.fr>
2022-05-01 14:35:30 +0200

    Experiments: Add wavread method to utils file

--- code/experiments/utils_functions.py
+++ code/experiments/utils_functions.py
@@ -1,4 +1,6 @@
 import numpy as np
+import os
+from scipy.io.wavfile import read
 
 def generateSineSignal(frequency = 220.0, length=1.0, fs=44100.0):
     '''
@@ -13,3 +15,28 @@ def generateSineSignal(frequency = 220.0, length=1.0, fs=44100.0):
 
 def generateRampEnvelope(minimum_amplitude = 0.0, maximum_amplitude = 1.0, signal_length = 1.0, sample_rate = 44100.0):
     return np.linspace(minimum_amplitude, maximum_amplitude, int(1 / signal_length * sample_rate))
+
+INT16_FAC = (2**15)-1
+INT32_FAC = (2**31)-1
+INT64_FAC = (2**63)-1
+norm_factor = {'int16':INT16_FAC, 'int32':INT32_FAC, 'int64':INT64_FAC, 'float32':1.0, 'float64':1.0}  
+
+def wavread(filepath):
+    """
+    Read a wav file and convert it to a normalized floating point array
+    filepath: path to file on disk to read (mono or stereo)
+    returns sample_rate of file, x: floating point array, num_channels: number of channels of audio data
+    """
+
+    if(os.path.isfile(filepath) == False):
+        raise ValueError("Invalid input filepath", filepath)
+
+    sample_rate, data = read(filepath)
+
+    # Scale and convert audio into range -1 to 1
+    if len(data.shape) == 1 or len(data.shape) == 2:
+        data = np.float32(data) / norm_factor[data.dtype.name]
+    else:
+        raise ValueError("Audio file should be mono or stereo, not ", len(data.shape), " channels")
+
+    return sample_rate, data, 1 if len(data.shape) == 1 else data.shape[1]
