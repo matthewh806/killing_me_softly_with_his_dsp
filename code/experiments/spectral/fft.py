fe3e6ebcd66336a0fddb03b7ea6a86e860cd4677
matthewh806 <matthew.harris@ircam.fr>
2022-05-04 13:16:41 +0200

    Experiments:: Move real fft code into spectral directory

--- code/experiments/spectral/fft.py
+++ code/experiments/spectral/fft.py
@@ -1,6 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fft import fft, fftfreq
+from scipy.fft import fft, fftfreq, rfft, rfftfreq
+
+def db_fft(in_data, sample_rate):
+    """
+    Performs a real FFT on the data supplied, uses a hanning window to weight the data appropriately
+    in_data: 1 dimensional normalized ([-1, 1]) numpy array of data, sample_rate: sample rate of the input audio
+
+    Note: Creates a copy of the in_data for the purposes of windowing so that we don't modify the input data
+    This is less efficient than reversing the window operation - but at least we know the in_data is totally unmodified
+
+    returns:
+        frequencies: array values based on bin centers calculated using the sample rate
+        magnitude_dbs: magnitude array of the fft data calculated using 10*log10(magnitude spectrum)
+
+        The returned array sizes are len(data) // 2 + 1
+
+    """
+    data = np.array(in_data, copy=True)
+    data_length = len(data)
+    weighting = np.hanning(data_length)
+    data *= weighting
+
+    if not np.all(np.isfinite(data)):
+        np.nan_to_num(data, copy=False, nan=0.0, posinf=0.0, neginf=0.0)
+
+    fft_values = rfft(data)
+    frequencies = rfftfreq(data_length, d=1/sample_rate)
+    magnitude_spectrum = np.abs(fft_values) * 2 / np.sum(weighting)
+    magnitude_dbs = 10*np.log10(magnitude_spectrum)
+
+    return frequencies, magnitude_dbs
 
 if __name__ == "__main__":
     N = 600 # no sample points.
