c2890b86a1cab44b269ea8a031b51fdbbd30f1ae
matthewh806 <matthew.harris@ircam.fr>
2022-05-12 23:45:33 +0200

    Experiments::Steganography add basic transport functionality

--- code/experiments/steganography/frequency_domain_GUI.py
+++ code/experiments/steganography/frequency_domain_GUI.py
@@ -9,11 +9,17 @@ import numpy as np
 import json
 import frequency_domain
 
+FILE_DIR = os.path.dirname(os.path.realpath(__file__))
+ICONS_DIRECTORY = os.path.join(FILE_DIR, "../assets/icons")
+
 class FrequencySteganographyGUI:
     def __init__(self, master):
         self.master = master
         master.title("Frequency Domain Steganography")
         master.geometry("600x300")
+
+        self.play_icon = tk.PhotoImage(file = os.path.join(ICONS_DIRECTORY, "play_small.png"))
+        self.stop_icon = tk.PhotoImage(file = os.path.join(ICONS_DIRECTORY, "stop_small.png"))
 
         # load config file if it exists
         
@@ -38,23 +44,41 @@ class FrequencySteganographyGUI:
         transmitter_label.pack(side=tk.TOP)
 
         self.base_sound_path = tk.StringVar(transmitter_frame)
-        self.base_sound_path_button = tk.Button(transmitter_frame, text="Select Base Sound", command=partial(self.browse_sound, self.base_sound_path))
+        self.base_sound_path_button = tk.Button(transmitter_frame, 
+                                                text="Select Base Sound", 
+                                                command=partial(self.browse_sound, self.base_sound_path))
         self.base_sound_path_button.pack()
 
         self.secret_message_path = tk.StringVar(transmitter_frame)
-        self.secret_message_path_button = tk.Button(transmitter_frame, text="Select Secret Message", command=partial(self.browse_sound, self.secret_message_path))
+        self.secret_message_path_button = tk.Button(transmitter_frame, 
+                                                    text="Select Secret Message", 
+                                                    command=partial(self.browse_sound, self.secret_message_path))
         self.secret_message_path_button.pack()
 
-        self.record_secret_message_button = tk.Button(transmitter_frame, text="Record Secret Message", command=self.record_secret_message)
+        self.record_secret_message_button = tk.Button(transmitter_frame, 
+                                                        text="Record Secret Message", 
+                                                        command=self.record_secret_message)
         self.record_secret_message_button.pack()
 
-        self.hide_message_button = tk.Button(transmitter_frame, text="Hide Message", command=self.hide_message, state='disabled')
+        self.hide_message_button = tk.Button(transmitter_frame, 
+                                                text="Hide Message", 
+                                                command=self.hide_message, 
+                                                state='disabled')
         self.hide_message_button.pack()
 
-        self.play_combined_sound_button = tk.Button(transmitter_frame, text="Play combined sound", command=self.play_combined_sound, state='disabled')
+        self.play_combined_sound_button = tk.Button(transmitter_frame, 
+                                                    text="Play combined sound", 
+                                                    image=self.play_icon, 
+                                                    width=32,
+                                                    height=32,
+                                                    command=self.play_combined_sound, 
+                                                    state='disabled')
         self.play_combined_sound_button.pack()
 
-        self.save_combined_sound_button = tk.Button(transmitter_frame, text="Save combined sound", command=self.save_combined_sound, state='disabled')
+        self.save_combined_sound_button = tk.Button(transmitter_frame, 
+                                                    text="Save combined sound", 
+                                                    command=self.save_combined_sound, 
+                                                    state='disabled')
         self.save_combined_sound_button.pack()
         
         # RECEIVER UI
@@ -64,21 +88,32 @@ class FrequencySteganographyGUI:
         receiver_label.pack()
 
         self.steganographed_sound_path = tk.StringVar(receiver_frame)
-        self.load_steganographed_sound_path_button = tk.Button(receiver_frame, text="Select Steganographed Sound", command=partial(self.browse_sound, self.steganographed_sound_path))
+        self.load_steganographed_sound_path_button = tk.Button(receiver_frame, 
+                                                                text="Select Steganographed Sound", 
+                                                                command=partial(self.browse_sound, self.steganographed_sound_path))
         self.load_steganographed_sound_path_button.pack()
 
-        self.recover_message_button = tk.Button(receiver_frame, text="Recover Message", command=self.recover_message, state='disabled')
+        self.recover_message_button = tk.Button(receiver_frame, 
+                                                text="Recover Message", 
+                                                command=self.recover_message, 
+                                                state='disabled')
         self.recover_message_button.pack()
 
-        self.play_recovered_message_button = tk.Button(receiver_frame, text="Play hidden message", command=self.play_recovered_sound, state='disabled')
+        self.play_recovered_message_button = tk.Button(receiver_frame, 
+                                                        text="Play hidden message", 
+                                                        command=self.play_recovered_sound, 
+                                                        state='disabled')
         self.play_recovered_message_button.pack()
 
-        self.save_recovered_sound_button = tk.Button(receiver_frame, text="Save recovered sound", command=self.save_recovered_sound, state='disabled')
+        self.save_recovered_sound_button = tk.Button(receiver_frame, 
+                                                        text="Save recovered sound", 
+                                                        command=self.save_recovered_sound, 
+                                                        state='disabled')
         self.save_recovered_sound_button.pack()
 
 
     def browse_sound(self, out_path):
-        input_directory = self.most_recent_input_directory if hasattr(self, 'most_recent_input_directory') else os.path.dirname(os.path.realpath(__file__))
+        input_directory = self.most_recent_input_directory if hasattr(self, 'most_recent_input_directory') else FILE_DIR
         filename = fd.askopenfilename(title="Open audio file", initialdir=input_directory, filetypes=(('wav files', '*.wav'),))
         self.most_recent_input_directory = os.path.dirname(filename)
         out_path.set(filename)
@@ -87,7 +122,7 @@ class FrequencySteganographyGUI:
 
     def record_secret_message(self):
         if self.sound_recorder.recording():
-            self.secret_message_path.set(os.path.join(os.path.dirname(os.path.realpath(__file__)), "secret_message.wav"))
+            self.secret_message_path.set(os.path.join(FILE_DIR, "secret_message.wav"))
             self.sound_recorder.stop_recording()
             self.sound_recorder.save_recording(self.secret_message_path.get())
         else:
@@ -98,7 +133,7 @@ class FrequencySteganographyGUI:
     
     def hide_message(self):
         # TODO: Check paths are valid & mono
-        self.steganographed_sound_path.set(os.path.join(os.path.dirname(os.path.realpath(__file__)), "steganographed_audio.wav"))
+        self.steganographed_sound_path.set(os.path.join(FILE_DIR, "steganographed_audio.wav"))
         self.transmitter = frequency_domain.Transmitter(self.base_sound_path.get(), self.secret_message_path.get(), lpf_cutoff=14000.0, order=96)
         self.transmitter.perform()
         self.transmitter.write(self.steganographed_sound_path.get())
@@ -118,7 +153,7 @@ class FrequencySteganographyGUI:
         if not hasattr(self.transmitter, 'combined_signal'):
             return
 
-        self.play_sound(self.transmitter.combined_signal)
+        self.play_stop_sound(self.transmitter.combined_signal)
 
 
     def play_recovered_sound(self):
@@ -128,21 +163,23 @@ class FrequencySteganographyGUI:
         if not hasattr(self.receiver, "recovered_message_signal"):
             return
 
-        self.play_sound(self.receiver.recovered_message_signal)
+        self.play_stop_sound(self.receiver.recovered_message_signal)
 
 
-    def play_sound(self, numpy_array):
+    def play_stop_sound(self, numpy_array):
         if self.numpy_player.processing():
-            return
-
-        self.numpy_player.start_processing_non_blocking(numpy_array, sample_rate=44100)
+            self.numpy_player.stop_processing()
+            self.update_button_states()
+        else:
+            self.numpy_player.start_processing_non_blocking(numpy_array, sample_rate=44100)
+            self.update_button_states()
 
 
     def save_combined_sound(self):
         if not hasattr(self, 'transmitter'):
             return
 
-        output_directory = self.most_recent_output_directory if hasattr(self, 'most_recent_output_directory') else os.path.dirname(os.path.realpath(__file__))
+        output_directory = self.most_recent_output_directory if hasattr(self, 'most_recent_output_directory') else FILE_DIR
         path = fd.asksaveasfilename(initialdir=output_directory)
         self.most_recent_output_directory = os.path.dirname(path)
         self.transmitter.write(path, True)
@@ -152,7 +189,7 @@ class FrequencySteganographyGUI:
         if not self.receiver:
             return
 
-        output_directory = self.most_recent_output_directory if hasattr(self, 'most_recent_output_directory') else os.path.dirname(os.path.realpath(__file__))
+        output_directory = self.most_recent_output_directory if hasattr(self, 'most_recent_output_directory') else FILE_DIR
         path = fd.asksaveasfilename(initialdir=output_directory)
         self.most_recent_output_directory = os.path.dirname(path)
         self.receiver.write(path)
@@ -179,10 +216,17 @@ class FrequencySteganographyGUI:
         self.play_recovered_message_button.config(state='normal' if performed_recovery else 'disabled')
         self.save_recovered_sound_button.config(state='normal' if performed_recovery else 'disabled')
 
+        if self.numpy_player.processing():
+            self.play_combined_sound_button.config(image=self.stop_icon)
+            self.play_recovered_message_button.config(image=self.stop_icon)
+        else:
+            self.play_combined_sound_button.config(image=self.play_icon)
+            self.play_recovered_message_button.config(image=self.play_icon)
+
 
     def save_config_file(self):
         # store in current directory for now
-        current_file_directory = os.path.dirname(os.path.realpath(__file__))
+        current_file_directory = FILE_DIR
         config_file_path = os.path.join(current_file_directory, ".config.json")
 
         current_config = self.load_config_file()
@@ -196,8 +240,8 @@ class FrequencySteganographyGUI:
             }
         else:
             data = {
-                'input_file_directory': self.most_recent_input_directory if hasattr(self, 'most_recent_input_directory') else os.path.dirname(os.path.realpath(__file__)),
-                'output_file_directory': self.most_recent_output_directory if hasattr(self, 'most_recent_output_directory') else os.path.dirname(os.path.realpath(__file__))
+                'input_file_directory': self.most_recent_input_directory if hasattr(self, 'most_recent_input_directory') else FILE_DIR,
+                'output_file_directory': self.most_recent_output_directory if hasattr(self, 'most_recent_output_directory') else FILE_DIR
             }
 
         with open(config_file_path, 'w') as json_file:
@@ -206,7 +250,7 @@ class FrequencySteganographyGUI:
 
     def load_config_file(self):
         # returns json object
-        current_file_directory = os.path.dirname(os.path.realpath(__file__))
+        current_file_directory = FILE_DIR
         config_file_path = os.path.join(current_file_directory, ".config.json")
         if not os.path.exists(config_file_path):
             return None
