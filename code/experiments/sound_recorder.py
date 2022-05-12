38040077f8a8266aa993c14a40fbccf5985b1d46
matthewh806 <matthew.harris@ircam.fr>
2022-05-12 22:34:36 +0200

    Experiments: turn sound_recorder into a non blocking class

--- code/experiments/sound_recorder.py
+++ code/experiments/sound_recorder.py
@@ -4,51 +4,107 @@ import utils_functions as UF
 
 class SoundRecorder:
     '''
+    A simple sound recorder class based around pyaudio for recording a mono wav file from 
+    the default input device
+
+    Note:
+        - Mono
+        - records float_32 format
+        - fixed sample rate (44100 Hz)
+        - fixed (default) input device
     '''
+
     def __init__(self):
         self.paudio = pyaudio.PyAudio()
         self.sample_rate = 44100
         self.channels = 1
         self.buffer_size = 512
         self.record_length = 5.0
+        self.recorded_frames = np.array([], dtype=np.float32)
 
-        print(self.paudio.get_default_host_api_info())
-        numdevices = self.paudio.get_default_host_api_info().get('deviceCount')
-        for i in range(0, numdevices):
-            if self.paudio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels') > 0:
-                print("Input Device id ", i, " - ", self.paudio.get_device_info_by_host_api_device_index(0, i).get('name'))
-                
+        # For querying the device
+        # print(self.paudio.get_default_host_api_info())
+        # numdevices = self.paudio.get_default_host_api_info().get('deviceCount')
+        # for i in range(0, numdevices):
+        #     if self.paudio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels') > 0:
+        #         print("Input Device id ", i, " - ", self.paudio.get_device_info_by_host_api_device_index(0, i).get('name'))
 
-    def start_recording(self):
+
+    def start_recording_non_blocking(self):
+        '''
+        Start recording audio from the default input device
+
+        This will continue streaming audio input until the stop_recording() method is called
+        Note: This process is non-blocking meaning it will be called on a different audio thread
+        to the main one
+        '''
+
+        self.recorded_frames = np.array([], dtype=np.float32)
         self.stream = self.paudio.open(format=pyaudio.paFloat32,
-                                        channels=1,
+                                        channels=self.channels,
                                         rate=self.sample_rate,
                                         frames_per_buffer=self.buffer_size,
                                         input=True,
                                         output=False,
-                                        input_device_index=None)
+                                        input_device_index=None,
+                                        stream_callback=self._pyaudio_callback)
 
-        #self.stream.start_stream()
-        self.recorded_frames = np.array([], dtype=np.float32)
-
-        for i in range(0, int(self.sample_rate / self.buffer_size * self.record_length)):
-            data = self.stream.read(self.buffer_size)
-            self.recorded_frames = np.append(self.recorded_frames, np.frombuffer(data, dtype=np.float32))
-
-        self.stream.stop_stream()
-        self.stream.close()
+        print("Start Recording")
+        self.stream.start_stream()
 
 
     def save_recording(self, output_file_path):
+        '''
+        Save the recording as a wav file to output_file_path
+        '''
         UF.wavwrite(self.recorded_frames, sample_rate=self.sample_rate, filepath=output_file_path)
 
 
+    def recording(self):
+        '''
+        Checks if pyaudio is current recording
+
+        Returns True / False
+        '''
+        if not hasattr(self, "stream"):
+            return False
+
+        if self.stream is None:
+            return False
+
+        return self.stream.is_active()
+
+    
+    def stop_recording(self):
+        '''
+        If streaming will stop any current recording and close the stream object
+        '''
+        if not hasattr(self, "stream"):
+            return
+
+        self.stream.stop_stream()
+        self.stream.close()
+        self.stream = None
+
+    
+    def _pyaudio_callback(self, in_data, frame_count, time_info, status):
+        self.recorded_frames = np.append(self.recorded_frames, np.frombuffer(in_data, dtype=np.float32))
+        return in_data, pyaudio.paContinue
+
+
 if __name__ == "__main__":
-    import os
+    import os, time
     current_file_directory = os.path.dirname(os.path.realpath(__file__))
     output_sounds_directory = os.path.join(current_file_directory, "sounds/output_sounds")
     output_sound_path = os.path.join(output_sounds_directory, "test_recording.wav")
 
     sound_recorder = SoundRecorder()
-    sound_recorder.start_recording()
+    sound_recorder.start_recording_non_blocking()
+
+    try:
+        while sound_recorder.recording():
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print("Stopped Recording")
+
     sound_recorder.save_recording(output_sound_path)
