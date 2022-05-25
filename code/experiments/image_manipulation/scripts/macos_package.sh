2a00f1c2479806f80812a77e04bf5c99fd37803f
matthewh806 <matthew.harris@ircam.fr>
2022-05-25 16:33:20 +0200

    Experiments::image_manipulation: update version to 0.0.2

--- code/experiments/image_manipulation/scripts/macos_package.sh
+++ code/experiments/image_manipulation/scripts/macos_package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 APP_NAME="SoxMosh"
-APP_VERSION="0.0.1"
+APP_VERSION="0.0.2"
 CURRENT_PATH="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILD_PATH="$CURRENT_PATH/../dist"
 OUTPUT_PATH="$CURRENT_PATH/../$APP_NAME"
