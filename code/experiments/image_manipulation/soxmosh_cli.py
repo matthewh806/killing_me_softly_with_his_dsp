1aeb07b3f57c99944fa6a4b702196c2c79670850
matthewh806 <matthew.harris@ircam.fr>
2022-05-28 02:13:36 +0200

    Experiments::image_manipulation: Modify CLI to take additional gif argument

--- code/experiments/image_manipulation/soxmosh_cli.py
+++ code/experiments/image_manipulation/soxmosh_cli.py
@@ -12,21 +12,28 @@ Requires the user to supply an input image path, output image path and an effect
 The sample rate to use for the image transformations can optionally be specified with 
 the --sample-rate parameter. If this is not provided the default of 44100 Hz will be used
 
+Supply the parameter --gif to make an animated gif (note this requires a slightly different 
+json structure - see examples). The optional parameter supplied with --gif specifies the
+individual image frame times (in ms) for the animation
+
 Its possible to use the class directly bypassing the json approach, look at
-the __main__ block in soxmosh.py for an example
+examples.py for lots of use cases
 '''
 
 
-def main(input_image_path, output_image_path, effects_data_path, sample_rate):
+def run_cli(input_image_path, output_image_path, effects_data_path, sample_rate, gif_duration=None):
     with open(effects_data_path, "r") as json_file:
         data = json.load(json_file)
 
-    sox_mosh = SoxMosh(input_path=input_image_path,
-                       output_path=output_image_path, sample_rate=sample_rate)
-    sox_mosh.databend_image(data['effects'])
+    sox_mosh = SoxMosh(input_path=input_image_path, sample_rate=sample_rate)
+
+    if not gif_duration:
+        sox_mosh.databend_image(output_image_path, data['effects'])
+    else:
+        sox_mosh.databend_to_gif(output_image_path, data['effects'], duration=gif_duration)
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "input_image", help="The path to the input image to be datamoshed")
@@ -36,7 +43,10 @@ if __name__ == "__main__":
         "--sample-rate", default=44100, type=int, help="The sample rate to use for the effects"
     )
     parser.add_argument("effects", help="The path to the effects json file")
-    parser.add_argument("--log", default="INFO", help="Set the logging level to be used for stdout")
+    parser.add_argument("--gif", type=int, const=30, nargs="?",
+                        help="Generate a gif made of frames of individual moshed images. The value supplied corresponds to the individual frame time in ms")
+    parser.add_argument("--log", default="INFO",
+                        help="Set the logging level to be used for stdout")
     args = parser.parse_args()
 
     numeric_level = getattr(logging, args.log.upper(), None)
@@ -44,5 +54,9 @@ if __name__ == "__main__":
         raise ValueError('Invalid log level: %s' % args.log)
     logging.basicConfig(level=numeric_level)
 
-    main(input_image_path=args.input_image,
-         output_image_path=args.output_image, effects_data_path=args.effects, sample_rate=args.sample_rate)
+    run_cli(input_image_path=args.input_image,
+            output_image_path=args.output_image, effects_data_path=args.effects, sample_rate=args.sample_rate, gif_duration=args.gif)
+
+
+if __name__ == "__main__":
+    main()
