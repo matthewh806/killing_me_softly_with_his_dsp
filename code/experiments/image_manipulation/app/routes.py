b8011137635a927f4cbe1ae3142384cb01f60f54
matthewh806 <matthew.harris@ircam.fr>
2022-06-08 16:39:43 +0200

    Experiments::image_manipulation: expose json effect field

--- code/experiments/image_manipulation/app/routes.py
+++ code/experiments/image_manipulation/app/routes.py
@@ -1,19 +1,27 @@
 from app import app
-from flask import render_template, url_for
+from app.forms import MoshForm
+from flask import render_template, url_for, redirect, request
 from soxmosh import SoxMosh
 import os
+import json
 
 CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', input_path=url_for('static', filename='perfect_blue_face.bmp'))
+    form = MoshForm()
+    return render_template('index.html', input_path=url_for('static', filename='perfect_blue_face.bmp'), form=form)
 
-@app.route("/mosh_image/", methods=['POST'])
+@app.route("/mosh_image", methods=['GET','POST'])
 def mosh_image():
-    sox_mosh = SoxMosh(os.path.join(CURRENT_DIRECTORY, 'static/perfect_blue_face.bmp'))
-    sox_mosh.databend_image(os.path.join(CURRENT_DIRECTORY, 'static/perfect_blue_face_out.bmp'),
-                        [{"echos": {"gain_in": 0.2, "gain_out": 0.88, "delays": [60], "decays": [0.5]}}])
-                        
-    return render_template('index.html', input_path=url_for('static', filename='perfect_blue_face.bmp'), output_path=url_for('static', filename='perfect_blue_face_out.bmp'))
+    form = MoshForm()
+    
+    if request.method == 'POST':
+        effects_json = json.loads(request.form['effects'])
+        sox_mosh = SoxMosh(os.path.join(CURRENT_DIRECTORY, 'static/perfect_blue_face.bmp'))
+        sox_mosh.databend_image(os.path.join(CURRENT_DIRECTORY, 'static/perfect_blue_face_out.bmp'), effects_json)
+
+        return render_template('index.html', input_path=url_for('static', filename='perfect_blue_face.bmp'), output_path=url_for('static', filename='perfect_blue_face_out.bmp'), form=form)
+
+    return render_template('index.html', input_path=url_for('static', filename='perfect_blue_face.bmp'), output_path=url_for('static', filename='perfect_blue_face_out.bmp'), form=form)              
