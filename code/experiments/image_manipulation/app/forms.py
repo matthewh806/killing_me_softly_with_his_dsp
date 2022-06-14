8c42ce66d5ab472269baf6984f7f420dc8868df9
matthewh806 <matthew.harris@ircam.fr>
2022-06-14 19:52:47 +0200

    Experiments::image_manipulation: support for gifs in webapp

--- code/experiments/image_manipulation/app/forms.py
+++ code/experiments/image_manipulation/app/forms.py
@@ -1,9 +1,11 @@
+from email.policy import default
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
+from wtforms import TextAreaField, SubmitField, BooleanField
 from flask_wtf.file import FileField, FileRequired
 
 class MoshForm(FlaskForm):
     effects = TextAreaField('effects', default='[{"echos": {"gain_in": 0.8, "gain_out": 0.01, "delays": [60], "decays": [0.5]}}]')
+    rendergif = BooleanField("Create GIF", default=False)
     submit = SubmitField('Mosh Image')
 
 class UploadForm(FlaskForm):
