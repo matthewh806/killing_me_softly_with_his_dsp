e6cadaea5158c658fb6aaa2722101cc1388accf3
matthewh806 <matthew.harris@ircam.fr>
2022-06-14 17:20:21 +0200

    Experiments::image_manipulation: Add support for image upload in webapp

--- code/experiments/image_manipulation/app/routes.py
+++ code/experiments/image_manipulation/app/routes.py
@@ -1,27 +1,64 @@
 from app import app
-from app.forms import MoshForm
-from flask import render_template, url_for, redirect, request
+from app.forms import MoshForm, UploadForm
+from flask import render_template, url_for, redirect, request, session
+from werkzeug.utils import secure_filename
 from soxmosh import SoxMosh
 import os
 import json
 
 CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+DEFAULT_FILE = os.path.join(CURRENT_DIRECTORY, "static/perfect_blue_face.bmp")
+
+ALLOWED_EXTENSIONS = {'bmp', 'png'}
 
 @app.route('/')
 @app.route('/index')
 def index():
-    form = MoshForm()
-    return render_template('index.html', input_path=url_for('static', filename='perfect_blue_face.bmp'), form=form)
+
+    if 'upload_filename' in session:
+        input_path = url_for('static', filename='uploads/' + session['upload_filename'])
+    else:
+        input_path = url_for('static', filename='perfect_blue_face.bmp')
+
+    upload_form = UploadForm()
+    mosh_form = MoshForm()
+    return render_template('index.html', input_path=input_path, upload_form=upload_form, mosh_form=mosh_form)
+
 
 @app.route("/mosh_image", methods=['GET','POST'])
 def mosh_image():
-    form = MoshForm()
+    upload_form = UploadForm()
+    mosh_form = MoshForm()
+
+    if 'upload_filename' in session:
+        input_path = os.path.join(CURRENT_DIRECTORY, 'static/uploads/' + session['upload_filename'])
+        input_url = url_for('static', filename='uploads/' + session['upload_filename'])
+        output_path = os.path.join(CURRENT_DIRECTORY, 'static/output/' + session['upload_filename'])
+        output_url = url_for('static', filename='output/' + session['upload_filename'])
+    else:
+        input_path = os.path.join(CURRENT_DIRECTORY, 'static/perfect_blue_face.bmp')
+        input_url = url_for('static', filename='perfect_blue_face.bmp')
+        output_path = os.path.join(CURRENT_DIRECTORY, 'static/perfect_blue_face_out.bmp')
+        output_url = url_for('static', filename='output/perfect_blue_face_out.bmp')
     
     if request.method == 'POST':
         effects_json = json.loads(request.form['effects'])
-        sox_mosh = SoxMosh(os.path.join(CURRENT_DIRECTORY, 'static/perfect_blue_face.bmp'))
-        sox_mosh.databend_image(os.path.join(CURRENT_DIRECTORY, 'static/perfect_blue_face_out.bmp'), effects_json)
+        sox_mosh = SoxMosh(input_path)
+        sox_mosh.databend_image(output_path, effects_json)
 
-        return render_template('index.html', input_path=url_for('static', filename='perfect_blue_face.bmp'), output_path=url_for('static', filename='perfect_blue_face_out.bmp'), form=form)
+        return render_template('index.html', input_path=input_url, output_path=output_url, upload_form=upload_form, mosh_form=mosh_form)
 
-    return render_template('index.html', input_path=url_for('static', filename='perfect_blue_face.bmp'), output_path=url_for('static', filename='perfect_blue_face_out.bmp'), form=form)              
+    return render_template('index.html', input_path=input_url, output_path=output_url, upload_form=upload_form, mosh_form=mosh_form)              
+
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    form = UploadForm()
+
+    if form.validate_on_submit():
+        f = form.image.data
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        session['upload_filename'] = filename
+    
+    return redirect(url_for('index'))
