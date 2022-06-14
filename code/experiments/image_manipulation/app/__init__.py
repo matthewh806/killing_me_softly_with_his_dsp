224dd3d8cbdff601bc876695c88718090e0c9d92
matthewh806 <matthew.harris@ircam.fr>
2022-06-14 17:20:21 +0200

    Experiments::image_manipulation: Add support for image upload in webapp

--- code/experiments/image_manipulation/app/__init__.py
+++ code/experiments/image_manipulation/app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import os
+
+CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config['UPLOAD_FOLDER'] = os.path.join(CURRENT_DIRECTORY, 'static/uploads')
+app.config['MAX_CONTENT_LENGTH'] = 30 * 1000 * 1000 # 30 mb
 
 from app import routes
--- code/experiments/image_manipulation/app/forms.py
+++ code/experiments/image_manipulation/app/forms.py
@@ -1,6 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, SubmitField
+from flask_wtf.file import FileField, FileRequired
 
 class MoshForm(FlaskForm):
     effects = TextAreaField('effects', default='[{"echos": {"gain_in": 0.8, "gain_out": 0.01, "delays": [60], "decays": [0.5]}}]')
     submit = SubmitField('Mosh Image')
+
+class UploadForm(FlaskForm):
+    image = FileField(validators=[FileRequired()])
+    submit = SubmitField('Upload Image')
