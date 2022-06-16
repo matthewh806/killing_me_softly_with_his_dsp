01763f4461c3162cb489af293576f554bc2f6544
matthewh806 <matthew.harris@ircam.fr>
2022-06-16 04:00:31 +0200

    Experiments::image_manipulation: background processing

--- code/experiments/image_manipulation/app/routes.py
+++ code/experiments/image_manipulation/app/routes.py
@@ -6,6 +6,8 @@ from soxmosh import SoxMosh
 import os
 import json
 
+from rq.job import Job
+
 CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 DEFAULT_FILE = os.path.join(CURRENT_DIRECTORY, "static/perfect_blue_face.bmp")
 
@@ -16,48 +18,48 @@ ALLOWED_EXTENSIONS = {'bmp', 'png'}
 def index():
 
     if 'upload_filename' in session:
-        input_path = url_for('static', filename='uploads/' + session['upload_filename'])
+        input_url = url_for('static', filename='uploads/' + session['upload_filename'])
+        
     else:
-        input_path = url_for('static', filename='perfect_blue_face.bmp')
+        input_url = url_for('static', filename='perfect_blue_face.bmp')
+
+    output_url = ""
+    if 'output_filename' in session:
+        output_url = url_for('static', filename='output/' + session['output_filename'])
 
     upload_form = UploadForm()
     mosh_form = MoshForm()
-    return render_template('index.html', input_path=input_path, upload_form=upload_form, mosh_form=mosh_form)
+    return render_template('index.html', input_path=input_url, output_path=output_url, upload_form=upload_form, mosh_form=mosh_form)
 
 
 @app.route("/mosh_image", methods=['GET','POST'])
 def mosh_image():
-    upload_form = UploadForm()
     mosh_form = MoshForm()
 
     if 'upload_filename' in session:
         input_path = os.path.join(CURRENT_DIRECTORY, 'static/uploads/' + session['upload_filename'])
-        input_url = url_for('static', filename='uploads/' + session['upload_filename'])
-        output_path = os.path.join(CURRENT_DIRECTORY, 'static/output/' + session['upload_filename'])
-        output_url = url_for('static', filename='output/' + session['upload_filename'])
     else:
         input_path = os.path.join(CURRENT_DIRECTORY, 'static/perfect_blue_face.bmp')
-        input_url = url_for('static', filename='perfect_blue_face.bmp')
-        output_path = os.path.join(CURRENT_DIRECTORY, 'static/perfect_blue_face_out.bmp')
-        output_url = url_for('static', filename='output/perfect_blue_face_out.bmp')
-    
+          
     if request.method == 'POST':
         effects_json = json.loads(request.form['effects'])
 
         if mosh_form.rendergif.data:
-            pre, _ = os.path.splitext(output_path)
-            output_path = pre + ".gif"
-
-            url_pre, _ = os.path.splitext(output_url)
-            output_url = url_pre + ".gif"
+            pre, _ = os.path.splitext(session['upload_filename'])
+            output_filename = pre + ".gif"
+            session['output_filename'] = output_filename
+            output_path = os.path.join(CURRENT_DIRECTORY, 'static/output/' + output_filename)
             job = current_app.task_queue.enqueue('app.tasks.generate_gif_task', input_path, output_path, effects_json)
+
+            return redirect(url_for('result', id=job.id))
         else:
-            sox_mosh = SoxMosh(input_path)
-            sox_mosh.databend_image(output_path, effects_json)
+            session['output_filename'] = session['upload_filename']
+            output_path = os.path.join(CURRENT_DIRECTORY, 'static/output/' + session['output_filename'])
+            job = current_app.task_queue.enqueue('app.tasks.generate_image_task', input_path, output_path, effects_json)
 
-        return render_template('index.html', input_path=input_url, output_path=output_url, upload_form=upload_form, mosh_form=mosh_form)
+            return redirect(url_for('result', id=job.id))
 
-    return render_template('index.html', input_path=input_url, output_path=output_url, upload_form=upload_form, mosh_form=mosh_form)              
+    return redirect(url_for('index'))       
 
 
 @app.route("/upload", methods=['POST'])
@@ -71,3 +73,16 @@ def upload():
         session['upload_filename'] = filename
     
     return redirect(url_for('index'))
+
+@app.route('/result/<string:id>')
+def result(id):
+    job = Job.fetch(id, connection=current_app.redis)
+    status = job.get_status()
+
+    if status in ['queued', 'started', 'deferred', 'failed']:
+        return render_template('processing.html', refresh=True)
+    elif status == 'finished':
+        print("finished the job!")
+        return redirect(url_for('index'))
+    
+
