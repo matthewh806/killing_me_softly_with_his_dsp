96bba7b9990212e9454bc8c7d208970174a54827
matthewh806 <matthew.harris@ircam.fr>
2022-06-23 00:42:31 +0200

    Experiments::image_manipulation add echo UI form

--- code/experiments/image_manipulation/app/forms.py
+++ code/experiments/image_manipulation/app/forms.py
@@ -1,10 +1,20 @@
 from email.policy import default
+from tkinter import Label
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField, BooleanField
+from wtforms import TextAreaField, SubmitField, BooleanField, FloatField, IntegerField, HiddenField, FieldList
 from flask_wtf.file import FileField, FileRequired
 
+class EchoForm(FlaskForm):
+    effect_name = HiddenField("echos")
+    gain_in = FloatField('In Gain', default=0.8)
+    gain_out = FloatField('Out Gain', default=0.9)
+    n_echos = IntegerField("Num echos", default=1)
+    delays = TextAreaField("Delays", default="60")
+    decays = TextAreaField("Decays", default="0.4")
+    submit = SubmitField("Add Effect")
+
 class MoshForm(FlaskForm):
-    effects = TextAreaField('effects', default='[{"echos": {"gain_in": 0.8, "gain_out": 0.01, "delays": [60], "decays": [0.5]}}]')
+    effects = TextAreaField('effects')
     rendergif = BooleanField("Create GIF", default=False)
     submit = SubmitField('Mosh Image')
 
--- code/experiments/image_manipulation/app/routes.py
+++ code/experiments/image_manipulation/app/routes.py
@@ -1,10 +1,11 @@
 from app import app
-from app.forms import MoshForm, UploadForm
+from app.forms import MoshForm, UploadForm, EchoForm
 from flask import render_template, url_for, redirect, request, session, current_app
 from werkzeug.utils import secure_filename
 from soxmosh import SoxMosh
 import os
 import json
+import wtforms_json
 
 from rq.job import Job
 
@@ -12,6 +13,10 @@ CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 DEFAULT_FILE = os.path.join(CURRENT_DIRECTORY, "static/perfect_blue_face.bmp")
 
 ALLOWED_EXTENSIONS = {'bmp', 'png'}
+
+DEFAULT_EFFECTS = '[{"echos": {"gain_in": 0.8, "gain_out": 0.5, "delays": [60], "decays": [0.5]}}]'
+
+wtforms_json.init()
 
 @app.route('/')
 @app.route('/index')
@@ -28,8 +33,16 @@ def index():
         output_url = url_for('static', filename='output/' + session['output_filename'])
 
     upload_form = UploadForm()
+    echo_form = EchoForm(effect_name='echos')
     mosh_form = MoshForm()
-    return render_template('index.html', input_path=input_url, output_path=output_url, upload_form=upload_form, mosh_form=mosh_form)
+
+    if 'effects_json' in session and not session['effects_json'] == '':
+        mosh_form.effects.data = session['effects_json']
+    else:
+        mosh_form.effects.data = DEFAULT_EFFECTS
+        session['effects_json'] = mosh_form.effects.data
+
+    return render_template('index.html', input_path=input_url, output_path=output_url, upload_form=upload_form, echo_form=echo_form, mosh_form=mosh_form)
 
 
 @app.route("/mosh_image", methods=['GET','POST'])
@@ -73,6 +86,45 @@ def upload():
         session['upload_filename'] = filename
     
     return redirect(url_for('index'))
+
+
+@app.route("/add_effect", methods=['POST'])
+def add_effect():
+    effect_dict = request.form.to_dict()
+    effect_name = effect_dict.pop('effect_name', None)
+
+    form = None
+    if effect_name == 'echos':
+        form = EchoForm()
+
+    effect_dict = {}
+    ignore_fields = ["effect_name", "submit", "csrf_token"]
+    for fieldname, value in form.data.items():
+        if not fieldname in ignore_fields:
+            if isinstance(value, str):
+                # This is a hacky way of dealing with inputs where a list could be passed
+                # On the form side we use a TextAreaField and expect the input to be comma separated
+                if "," in value:
+                    # convert csv to list - try to guess type?
+                    effect_str_values = value.split(",")
+                    effect_values = [int(float(x)) if int(float(x)) == float(x) else float(x) for x in effect_str_values]
+                    value = effect_values
+                else:
+                    # directly convert string to type 
+                    value = [int(float(value)) if int(float(value)) == float(value) else float(value)]
+
+            effect_dict[fieldname] = value
+
+    effect_json = {}
+    effect_json[effect_name] = effect_dict
+
+    effects_json_str = session['effects_json'] if 'effects_json' in session else []
+    effects_json_array = json.loads(effects_json_str)
+    effects_json_array.append(effect_json)
+    
+    session['effects_json'] = json.dumps(effects_json_array)
+    return redirect(url_for('index'))
+
 
 @app.route('/result/<string:id>')
 def result(id):
