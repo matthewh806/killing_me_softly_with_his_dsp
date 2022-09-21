5642ca86791d6d3b76f2516e8c780107bbef7256
matthew <harris@ircam.fr>
2022-09-21 22:30:29 +0200

    Experiments: Add square wave osc

--- code/experiments/synthesis/oscillator.py
+++ code/experiments/synthesis/oscillator.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-
+import math
 import numpy as  np
 
 class Oscillator(ABC):
@@ -35,7 +35,30 @@ class SineOscillator(Oscillator):
 
         return val * self.amp
 
+class SquareOscillator(Oscillator):
+    def _initialize_osc(self):
+        self._step = 1/self.sample_rate
+        self._i = 0
+
+    def __next__(self):
+        val = 2 * (2 * math.floor(self.freq*self._i) - math.floor(2*self.freq*self._i)) + 1
+        self._i += + self._step
+        return val * self.amp
+
 if __name__ == "__main__":
-    osc = SineOscillator(440, 1, 44100)
-    signal = osc.getNextBlockCallback(1024)
-    print(signal)
+    sine_osc = SineOscillator(440, 1, 44100)
+    sine_signal = sine_osc.getNextBlockCallback(1024)
+    # print(sine_signal)
+
+    sqr_osc = SquareOscillator(440, 1, 44100)
+    sqr_signal = sqr_osc.getNextBlockCallback(1024)
+    # print(sqr_signal)
+
+    import sys, os
+    sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../'))
+    import utils_functions as UF
+    import matplotlib.pyplot as plt
+
+    times = np.linspace(0, len(sqr_signal), len(sqr_signal))
+    UF.signal_plot(1,1,1, times, sqr_signal)
+    plt.show()
