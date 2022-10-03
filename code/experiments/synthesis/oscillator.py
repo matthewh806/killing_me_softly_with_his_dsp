8626fe0a8cd4395b442d599cc4144ce6cb3eb07a
matthew <harris@ircam.fr>
2022-10-03 15:14:21 +0200

    Experiments: add triangle and sawtooth realtime oscillators

--- code/experiments/synthesis/oscillator.py
+++ code/experiments/synthesis/oscillator.py
@@ -137,6 +137,44 @@ class SquareOscillator(Oscillator):
             
         return val * self._a
 
+class SawtoothOscillator(Oscillator):
+    def _initialize_osc(self):
+        self._i = 0
+    
+    def _post_freq_set(self):
+        self._period = self.sample_rate / self._f
+        self._post_phase_set()
+
+    def _post_phase_set(self):
+        self._p = ((self._p + 90)/360) * self._period
+        
+    def __next__(self):
+        div = (self._i +  self._p) / self._period
+        val = 2 * (div - math.floor(0.5 + div))
+        self._i += 1
+
+        if self._wave_range is not (-1,1):
+            val = self.squish_val(val, *self._wave_range)
+        
+        return val *self._a
+
+
+class TriangleOscillator(SawtoothOscillator):
+    '''
+    Basically just the absolute value of the Sawtooth wave
+    '''
+
+    def __next__(self):
+        div = (self._i + self._p) / self._period
+        val = 2 * (div - math.floor(0.5 + div))
+        val = (abs(val) - 0.5) * 2
+        self._i += 1
+
+        if self._wave_range is not (-1,1):
+            val = self.squish_val(val, *self._wave_range)
+        
+        return val *self._a
+
 if __name__ == "__main__":
     sine_osc = SineOscillator()
     sine_signal = sine_osc.getNextBlockCallback(1024)
