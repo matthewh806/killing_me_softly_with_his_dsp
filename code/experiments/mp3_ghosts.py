2be2871738d3528f3521a9b78edfa9bc7c56238d
matthew <harris@ircam.fr>
2022-11-15 15:23:24 +0100

    Experiments: mp3_ghosts plotting, logging, code improvements

--- code/experiments/mp3_ghosts.py
+++ code/experiments/mp3_ghosts.py
@@ -7,14 +7,18 @@ https://www.theghostinthemp3.com/theghostinthemp3.html
 Steps:
     1. Convert WAV to mp3
     2. Perform STFT of a chunk of the original & mp3
-    3. Compare the FFTs visually
-    4. Subtract the spectrum of the WAV from the MP3 to get the "residual"
-    5. Perform ISTFT to resynthesize the sound and recover the "ghost audio"
+    3. Subtract the spectrum of the WAV from the MP3 to get the "residual"
+    4. Perform ISTFT to resynthesize the sound and recover the "ghost audio"
+    5. Save ghost mp3 to output location
+    6. Plot everything
 
     TODO
         1. Handle case where there's more than 1 channel
         2. Handle case where bit depth != 16 (sample_width=2 assumed here!)
-        3. figure out a way to convert the in memory stream to mp3 rather than creating the intermediary file
+        3. Figure out a way to convert the in memory stream to mp3 rather than creating the intermediary file
+        4. Expose FFT params as arguments, 
+        5. Specify compression amount
+        6. CLI
 '''
 
 from pydub import AudioSegment
@@ -22,11 +26,14 @@ from scipy.signal import stft, istft
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import logging
+import tempfile
 
 CUR_DIR=os.path.dirname(os.path.realpath(__file__))
-INPUT_WAV=os.path.join(CUR_DIR, "../audio/sax-phrase.wav")
-INPUT_MP3=os.path.join(CUR_DIR, "../audio/output/sax-phrase.mp3")
-OUTPUT_GHOST=os.path.join(CUR_DIR, "../audio/output/sax-ghost.mp3")
+
+logging.basicConfig()
+logger = logging.getLogger('mp3_ghosts')
+logger.setLevel(level=logging.DEBUG)
 
 def generate_ghosts(input_path, output_path, start=0, end=-1):
     '''
@@ -50,83 +57,133 @@ def generate_ghosts(input_path, output_path, start=0, end=-1):
         The resulting ghost file is saved at output_path in mp3 format
     '''
 
+    # 1. Load uncompressed audio, create compressed mp3
     original_wav = AudioSegment.from_wav(input_path)
-    print("Original Size: ", len(original_wav.get_array_of_samples()))
-    print("Sample width: ", original_wav.sample_width)
+    logger.info("-----------INPUT INFO-----------")
+    logger.info("Generating audio data from: %s", input_path)
+    logger.info("Original Size: %i", len(original_wav.get_array_of_samples()))
+    logger.info("Sample Rate: %i Hz", original_wav.frame_rate)
+    logger.info("Channels: %i", original_wav.channels)
+    logger.info("Sample width: %i, Bit depth: %i", original_wav.sample_width, original_wav.sample_width*8)
+    logger.info("--------------------------------")
 
+    # Use signal length if end not specified 
     if end==-1:
-        end=len(original_wav.get_array_of_samples())
+        end=len(original_wav) # AudioSegment.__len__ returns length in ms
 
-    original_slice = original_wav[start:end]
-    print("Sliced Size: ", len(original_slice.get_array_of_samples()))  
+    if start >= end:
+        raise ValueError("Slice start must be < slice end")
 
-    orig_data = np.float32(np.array(original_slice.get_array_of_samples())) / (2**15 - 1)
-    original_slice.export(INPUT_MP3, format="mp3")
+    if start < 0:
+        logger.warn("Slice start < 0")
 
-    compressed_slice = AudioSegment.from_mp3(INPUT_MP3)
+    input_slice = original_wav[start:end]
+    logger.info("Extracting slice: start=%fs, end=%fs", start/1000.0, end/1000.0)
+    logger.info("Sliced Size: %i", len(input_slice.get_array_of_samples()))  
+    input_slice_data = np.float32(np.array(input_slice.get_array_of_samples())) / (2**15 - 1)
+
+    temp_file = tempfile.NamedTemporaryFile(suffix='.mp3')
+    input_slice.export(temp_file, format="mp3")
+
+    compressed_slice = AudioSegment.from_mp3(temp_file)
     compressed_data = np.float32(np.array(compressed_slice.get_array_of_samples())) / (2**15 - 1)
+    compressed_data = compressed_data[:len(input_slice.get_array_of_samples())] # force same size
 
-    print("original: ", orig_data)
-    print("compressed: ", compressed_data)
-
-    plt.figure(1, figsize=(9, 5))
-    plt.subplot(4,3,1)
-    plt.plot(orig_data, 'b', lw=1.5)
-
+    # 2. Perform STFTs of both original and compressed signal
     N=2048  # fft size
     M=128  # window size
     window = "hamming"
     overlap=4
 
-    (freqs_o, times_o, ys_o) = stft(orig_data, fs=original_wav.frame_rate, window=window, nfft=N, nperseg=M, noverlap=overlap, return_onesided=True)
-    print(ys_o.shape)
-    mX_orig = 20 * np.log10(np.abs(ys_o))
-    pX_orig = np.unwrap(np.angle(ys_o))
-    plt.subplot(3,3,2)
-    plt.pcolormesh(times_o, freqs_o, mX_orig, shading = 'flat')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Frequency (Hz)')
-    plt.subplot(3,3,3)
-    plt.pcolormesh(times_o, freqs_o, pX_orig, shading = 'flat')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Frequency (Hz)')
+    logger.info("-----------FFT INFO-------------")
+    logger.info("FFT size: %i", N)
+    logger.info("Window size: %i", M)
+    logger.info("Window type: %s", window)
+    logger.info("Overlap: %i", overlap)
+    logger.info("--------------------------------")
 
-    plt.subplot(3,3,4)
-    plt.plot(compressed_data, 'b', lw=1.5)    
-    plt.subplot(3,3,5)
+    (freqs_i, times_i, ys_i) = stft(input_slice_data, fs=original_wav.frame_rate, window=window, nfft=N, nperseg=M, noverlap=overlap, return_onesided=True)
+    mX_orig = 20 * np.log10(np.abs(ys_i))
+    pX_orig = np.unwrap(np.angle(ys_i))
+
     (freqs_c, times_c, ys_c) = stft(compressed_data, fs=original_wav.frame_rate, window=window, nfft=N, nperseg=M, noverlap=overlap, return_onesided=True)
     mX_comp = 20 * np.log10(np.abs(ys_c))
-    pX_comp = np.unwrap(np.angle(ys_o))
-    plt.pcolormesh(times_c, freqs_c, mX_comp, shading = 'flat')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Frequency (Hz)')
-    plt.subplot(3,3,6)
-    plt.pcolormesh(times_o, freqs_o, pX_comp, shading = 'flat')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Frequency (Hz)')
+    pX_comp = np.unwrap(np.angle(ys_c))
 
-    # Subtract the spectra and inverse fft to hear the ghost
-    mx_residual = mX_orig - mX_comp
-    px_residual = pX_orig - pX_comp
-    plt.subplot(3,3,8)
-    plt.pcolormesh(times_o, freqs_o, mx_residual)
-    plt.xlabel('Time (s)')
-    plt.ylabel('Frequency (Hz)')
+    # 3. Subtract the spectra
+    mX_residual = mX_orig - mX_comp
+    pX_residual = pX_orig - pX_comp
 
-    plt.subplot(3,3,9)
-    plt.pcolormesh(times_o, freqs_o, px_residual)
-    plt.xlabel('Time (s)')
-    plt.ylabel('Frequency (Hz)')
-
-    # resynthesize residual spectra (what to use for phase? subtraction? regen?)
-    residualY = 10**(mx_residual / 20) * np.exp(1j*pX_orig)
+    # 4. Resynthesize ghost spectra 
+    # (what to use for phase? subtraction? regen?)
+    residualY = 10**(mX_residual / 20) * np.exp(1j*pX_residual)
     (t_out, x_out) = istft(residualY, fs=original_wav.frame_rate, window=window, nperseg=M, noverlap=overlap, nfft=N, input_onesided=True)
-    plt.subplot(3,3,7)
-    plt.plot(t_out, x_out)
+
+    # 6. Save ghost mp3
+    y = np.int16(x_out * 2 ** 15)
+    ghost_slice = AudioSegment(y.tobytes(), frame_rate=original_wav.frame_rate, sample_width=2, channels=1)
+    ghost_slice.export(output_path, format="mp3")
+
+    # 5. plot everything
+    plt.figure(1, figsize=(15, 9))
+    times=np.linspace(start / 1000.0, end / 1000.0, num=len(input_slice_data))
+
+    # Uncompressed
+    plt.subplot(3,3,1)
     plt.xlabel('Time (s)')
     plt.ylabel('Amplitude')
+    plt.title('Uncompressed Signal')
+    plt.plot(times, input_slice_data)
+    plt.subplot(3,3,2)
+    plt.pcolormesh(times_i, freqs_i, mX_orig, shading='auto')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency (Hz)')
+    plt.title('Uncompressed Magnitude Spectrum')
+    plt.subplot(3,3,3)
+    plt.pcolormesh(times_i, freqs_i, pX_orig, shading='auto')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency (Hz)')
+    plt.title('Uncompressed Phase Spectrum')
+
+    # Compressed    
+    plt.subplot(3,3,4)
+    plt.plot(times, compressed_data)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude')    
+    plt.title('Compressed Signal')
+    plt.subplot(3,3,5)
+    plt.pcolormesh(times_c, freqs_c, mX_comp, shading='auto')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency (Hz)')
+    plt.title('Compressed Frequency Spectrum')
+    plt.subplot(3,3,6)
+    plt.pcolormesh(times_c, freqs_c, pX_comp, shading='auto')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency (Hz)')
+    plt.title('Compressed Phase Spectrum')
+
+    # Ghost
+    plt.subplot(3,3,7)
+    plt.plot(times, x_out[:len(input_slice_data)])
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude')
+    plt.title("Ghost Signal")
+    plt.subplot(3,3,8)
+    plt.pcolormesh(times_i, freqs_i, mX_residual, shading='auto')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency (Hz)')
+    plt.title('Ghost Magnitude Spectrum')
+    plt.title
+    plt.subplot(3,3,9)
+    plt.pcolormesh(times_i, freqs_i, pX_residual, shading='auto')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency (Hz)')
+    plt.title('Ghost Phase Spectrum')
+
+    plt.tight_layout()
     plt.show()
 
+    # JUNK
     # ys_residual = ys_o - ys_c
     # mx_residual = 20 * np.log10(np.abs(ys_residual))
     # plt.pcolormesh(times_o, freqs_o, mx_residual)
@@ -142,15 +199,12 @@ def generate_ghosts(input_path, output_path, start=0, end=-1):
     # plt.ylabel('Amplitude')
     # plt.show()
 
-    y = np.int16(x_out * 2 ** 15)
-    ghost_slice = AudioSegment(y.tobytes(), frame_rate=original_wav.frame_rate, sample_width=2, channels=1)
-    ghost_slice.export(output_path, format="mp3")
-
-
 if __name__ == "__main__":
     print("Boo")
+    input_file=os.path.join(CUR_DIR, "../audio/sax-phrase.wav")
 
-    START= 0.75 * 1000
-    END  = 1.25 * 1000
+    import pathlib
+    infile = pathlib.Path(input_file).stem
+    output_path=os.path.join(CUR_DIR, "../audio/output/{}_ghost.mp3".format(infile))
 
-    generate_ghosts(INPUT_WAV, OUTPUT_GHOST, START, END)
+    generate_ghosts(input_file, output_path, start=0.75 * 1000, end=1.25 * 1000)
