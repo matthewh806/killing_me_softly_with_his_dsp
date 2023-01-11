aad63592ef877c4e32fe8c197176816cec0de967
matthew <harris@ircam.fr>
2023-01-11 23:08:33 +0100

    Scripts:: Package VSTs and pulsar project for macos

--- code/scripts/package_macos_artifacts.sh
+++ code/scripts/package_macos_artifacts.sh
@@ -25,3 +25,20 @@ for d in $BUILD_PATH/applications/*; do
     fi
   fi
 done
+
+cd $BUILD_PATH/VST3/$BUILD_TYPE
+VSTFILENAME="KMSWHDSP_VSTs.zip"
+zip -r $VSTFILENAME *.vst3
+mv $VSTFILENAME $BUILD_PATH/artifacts
+
+# Repeition of above becuase of the extra Standalone dir.
+# TODO: Fix the building of this specific app! Its a standalone plugin rn.
+PULSAR_PATH="$BUILD_PATH/applications/pulsar/Pulsar_artefacts//$BUILD_TYPE/Standalone"
+cd $PULSAR_PATH
+# zip -r pulsar.zip "Pulsar Project.app"
+if [ -d *.app ]; then
+  # remove whitespace, add underscores, remove extension, make lower case
+  FILENAME=$(basename *.app .app | xargs | sed -e 's/ /_/g' | tr '[:upper:]' '[:lower:]')
+  zip -r $FILENAME.zip *.app
+  mv $FILENAME.zip $BUILD_PATH/artifacts
+fi
