d473e416f9808783d976cad0c4732c907cc3ad44
matthew <harris@ircam.fr>
2023-01-21 19:13:18 +0100

    Scripts:: Package up VST3s in a single zipped directory

--- code/scripts/package_macos_artifacts.sh
+++ code/scripts/package_macos_artifacts.sh
@@ -16,29 +16,32 @@ for d in $BUILD_PATH/applications/*; do
     if [ -d $d/*_artefacts ]; then
       cd $d/*_artefacts/$BUILD_TYPE
 
+      # This is for targets which are built as applications
       if [ -d *.app ]; then
         # remove whitespace, add underscores, remove extension, make lower case
         FILENAME=$(basename *.app .app | xargs | sed -e 's/ /_/g' | tr '[:upper:]' '[:lower:]')
         zip -r $FILENAME.zip *.app
         mv $FILENAME.zip $BUILD_PATH/artifacts
       fi
+
+      # This is for targets which are built as plugins
+      if [ -d $d/*_artefacts/$BUILD_TYPE/VST3 ]; then
+        cd $d/*_artefacts/$BUILD_TYPE/VST3
+        
+        if [ -d *.vst3 ]; then
+          # remove whitespace, add underscores, remove extension, make lower case
+          FILENAME=$(basename *.vst3 .vst3 | xargs | sed -e 's/ /_/g' | tr '[:upper:]' '[:lower:]')
+          mv *.vst3 $FILENAME.vst3
+          mv $FILENAME.vst3 $BUILD_PATH/VST3/$BUILD_TYPE
+        fi
+      fi
+
     fi
   fi
 done
 
+# Put all VST3's into a single zipped folder
 cd $BUILD_PATH/VST3/$BUILD_TYPE
 VSTFILENAME="KMSWHDSP_VSTs.zip"
 zip -r $VSTFILENAME *.vst3
 mv $VSTFILENAME $BUILD_PATH/artifacts
-
-# Repeition of above becuase of the extra Standalone dir.
-# TODO: Fix the building of this specific app! Its a standalone plugin rn.
-PULSAR_PATH="$BUILD_PATH/applications/pulsar/Pulsar_artefacts//$BUILD_TYPE/Standalone"
-cd $PULSAR_PATH
-# zip -r pulsar.zip "Pulsar Project.app"
-if [ -d *.app ]; then
-  # remove whitespace, add underscores, remove extension, make lower case
-  FILENAME=$(basename *.app .app | xargs | sed -e 's/ /_/g' | tr '[:upper:]' '[:lower:]')
-  zip -r $FILENAME.zip *.app
-  mv $FILENAME.zip $BUILD_PATH/artifacts
-fi
