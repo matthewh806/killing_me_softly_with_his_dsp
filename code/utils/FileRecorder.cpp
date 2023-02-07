dbfe489794b19fde9870b6036b4d90b07171d523
matthew <harris@ircam.fr>
2023-02-07 13:24:34 +0100

    Utils: Apply clang-format

--- code/utils/FileRecorder.cpp
+++ code/utils/FileRecorder.cpp
@@ -27,25 +27,25 @@ bool FileRecorder::startRecording(juce::File file, int numChannels, double sampl
         stopRecording();
         return false;
     }
-    
+
     if(!file.existsAsFile())
     {
         file.create();
     }
-    
+
     if(!file.existsAsFile())
     {
         stopRecording();
         return false;
     }
-    
+
     auto outputStream = std::unique_ptr<juce::FileOutputStream>(file.createOutputStream());
     if(outputStream == nullptr)
     {
         stopRecording();
         return false;
     }
-    
+
     auto writer = std::unique_ptr<juce::AudioFormatWriter>(audioFormat->createWriterFor(outputStream.get(), sampleRate, static_cast<unsigned int>(numChannels), bitDepth, {}, 0));
     if(writer == nullptr)
     {
@@ -53,36 +53,40 @@ bool FileRecorder::startRecording(juce::File file, int numChannels, double sampl
         return false;
     }
     outputStream.release();
-    
+
     auto threadedWriter = std::make_shared<juce::AudioFormatWriter::ThreadedWriter>(writer.get(), mBackgroundThread, 32768);
     if(threadedWriter == nullptr)
     {
         stopRecording();
         return false;
     }
-    
+
     threadedWriter = std::atomic_exchange(&mThreadedWriter, threadedWriter);
     mRecordingDuration = 0;
     writer.release();
-    
+
     juce::MessageManager::callAsync([ptr = std::move(threadedWriter)]()
                                     {
-                                        while(ptr.use_count() > 1) { }
+                                        while(ptr.use_count() > 1)
+                                        {
+                                        }
                                     });
-    
+
     std::cout << "Start Recording on background thread: " << file.getFullPathName() << "\n";
-    
+
     return true;
 }
 
 void FileRecorder::stopRecording()
 {
     std::cout << "Stop Recording on background thread\n";
-    
+
     juce::MessageManager::callAsync([ptr = std::move(mThreadedWriter)]()
-    {
-        while(ptr.use_count() > 1) { }
-    });
+                                    {
+                                        while(ptr.use_count() > 1)
+                                        {
+                                        }
+                                    });
 }
 
 void FileRecorder::processBlock(juce::AudioBuffer<float> const& buffer)
--- code/utils/FileRecorder.h
+++ code/utils/FileRecorder.h
@@ -13,23 +13,23 @@
 
 namespace OUS
 {
-  class FileRecorder
-  {
-  public:
-      FileRecorder(juce::AudioFormatManager& audioFormatManager);
-      virtual ~FileRecorder() = default;
-      
-      bool startRecording(juce::File file, int numChannels, double sampleRate, int bitDepth);
-      void stopRecording();
-      bool isRecording() const;
-      void processBlock(juce::AudioBuffer<float> const& buffer);
-      
-  private:
-      juce::AudioFormatManager& mFormatManager;
-      std::shared_ptr<juce::AudioFormatWriter::ThreadedWriter> mThreadedWriter;
-      juce::TimeSliceThread mBackgroundThread;
-      std::atomic<juce::int64> mRecordingDuration;
-      
-      JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(FileRecorder)
-  };
-}
+    class FileRecorder
+    {
+    public:
+        FileRecorder(juce::AudioFormatManager& audioFormatManager);
+        virtual ~FileRecorder() = default;
+
+        bool startRecording(juce::File file, int numChannels, double sampleRate, int bitDepth);
+        void stopRecording();
+        bool isRecording() const;
+        void processBlock(juce::AudioBuffer<float> const& buffer);
+
+    private:
+        juce::AudioFormatManager& mFormatManager;
+        std::shared_ptr<juce::AudioFormatWriter::ThreadedWriter> mThreadedWriter;
+        juce::TimeSliceThread mBackgroundThread;
+        std::atomic<juce::int64> mRecordingDuration;
+
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(FileRecorder)
+    };
+} // namespace OUS
