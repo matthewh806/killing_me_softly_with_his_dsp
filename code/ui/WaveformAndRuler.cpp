430cc227ac35c528d157467ca484025f52fe1079
matthew <harris@ircam.fr>
2023-02-25 17:59:05 +0100

    UI::WaveformRuler: add further forwarding methods

--- code/ui/WaveformAndRuler.cpp
+++ code/ui/WaveformAndRuler.cpp
@@ -24,9 +24,50 @@ WaveformAndRuler::~WaveformAndRuler()
     mWaveform.removeChangeListener(this);
 }
 
+WaveformComponent const& WaveformAndRuler::getWaveform() const
+{
+    return mWaveform;
+}
+
+void WaveformAndRuler::addWaveformChangeListener(juce::ChangeListener* listener)
+{
+    mWaveform.addChangeListener(listener);
+}
+
+void WaveformAndRuler::removeWaveformChangeListener(juce::ChangeListener* listener)
+{
+    mWaveform.removeChangeListener(listener);
+}
+
+juce::Range<float> const& WaveformAndRuler::getVisibleRange() const
+{
+    return mWaveform.getVisibleRange();
+}
+
+juce::Range<float> const& WaveformAndRuler::getTotalRange() const
+{
+    return mWaveform.getTotalRange();
+}
+
+void WaveformAndRuler::setSampleRate(float sampleRate)
+{
+    mSampleRuler.setSampleRate(sampleRate);
+}
+
 void WaveformAndRuler::resetZoom()
 {
     mWaveform.resetZoom();
+}
+
+// juce::FileDragAndDropTarget
+bool WaveformAndRuler::isInterestedInFileDrag(const StringArray& files)
+{
+    return mWaveform.isInterestedInFileDrag(files);
+}
+
+void WaveformAndRuler::filesDropped(const StringArray& files, int x, int y)
+{
+    mWaveform.filesDropped(files, x, y);
 }
 
 void WaveformAndRuler::setThumbnailSource(juce::AudioSampleBuffer* audioSource)
@@ -37,11 +78,21 @@ void WaveformAndRuler::setThumbnailSource(juce::AudioSampleBuffer* audioSource)
     repaint();
 }
 
+void WaveformAndRuler::clear()
+{
+    mWaveform.clear();
+}
+
 void WaveformAndRuler::resized()
 {
     auto bounds = getLocalBounds();
     mSampleRuler.setBounds(bounds.removeFromBottom(20));
     mWaveform.setBounds(bounds);
+}
+
+void WaveformAndRuler::mouseWheelMove(juce::MouseEvent const& event, juce::MouseWheelDetails const& wheel)
+{
+    mWaveform.mouseWheelMove(event, wheel);
 }
 
 void WaveformAndRuler::changeListenerCallback(juce::ChangeBroadcaster* source)
