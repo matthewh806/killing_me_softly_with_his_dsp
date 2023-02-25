0dc48291f3745bd650a34c3a1776447486108c24
matthew <harris@ircam.fr>
2023-02-25 17:59:05 +0100

    UI::WaveformRuler: add further forwarding methods

--- code/ui/WaveformAndRuler.cpp
+++ code/ui/WaveformAndRuler.cpp
@@ -16,7 +16,6 @@ WaveformAndRuler::WaveformAndRuler(juce::AudioFormatManager& formatManager)
     };
     
     addAndMakeVisible(mSampleRuler);
-    mSampleRuler.setSampleRate(44100.0);
 }
 
 WaveformAndRuler::~WaveformAndRuler()
@@ -24,9 +23,50 @@ WaveformAndRuler::~WaveformAndRuler()
     mWaveform.removeChangeListener(this);
 }
 
+WaveformComponent const& WaveformAndRuler::getWaveform() const
+{
+    return mWaveform;
+}
+
+void WaveformAndRuler::addWaveformChangeListener(juce::ChangeListener* listener)
+{
+    mWaveform.addChangeListener(listener);
+}
+
+void WaveformAndRuler::removeWaveformChangeListener(juce::ChangeListener* listener)
+{
+    mWaveform.removeChangeListener(listener);
+}
+
+juce::Range<float> const& WaveformAndRuler::getVisibleRange() const
+{
+    return mWaveform.getVisibleRange();
+}
+
+juce::Range<float> const& WaveformAndRuler::getTotalRange() const
+{
+    return mWaveform.getTotalRange();
+}
+
+void WaveformAndRuler::setSampleRate(float sampleRate)
+{
+    mSampleRuler.setSampleRate(sampleRate);
+}
+
 void WaveformAndRuler::resetZoom()
 {
     mWaveform.resetZoom();
+}
+
+// juce::FileDragAndDropTarget
+bool WaveformAndRuler::isInterestedInFileDrag(const StringArray& files)
+{
+    return mWaveform.isInterestedInFileDrag(files);
+}
+
+void WaveformAndRuler::filesDropped(const StringArray& files, int x, int y)
+{
+    mWaveform.filesDropped(files, x, y);
 }
 
 void WaveformAndRuler::setThumbnailSource(juce::AudioSampleBuffer* audioSource)
@@ -37,11 +77,21 @@ void WaveformAndRuler::setThumbnailSource(juce::AudioSampleBuffer* audioSource)
     repaint();
 }
 
+void WaveformAndRuler::clear()
+{
+    mWaveform.clear();
+}
+
 void WaveformAndRuler::resized()
 {
     auto bounds = getLocalBounds();
     mSampleRuler.setBounds(bounds.removeFromBottom(20));
     mWaveform.setBounds(bounds);
+}
+
+void WaveformAndRuler::mouseWheelMove(juce::MouseEvent const& event, juce::MouseWheelDetails const& wheel)
+{
+    mWaveform.mouseWheelMove(event, wheel);
 }
 
 void WaveformAndRuler::changeListenerCallback(juce::ChangeBroadcaster* source)
--- code/ui/WaveformAndRuler.h
+++ code/ui/WaveformAndRuler.h
@@ -8,6 +8,7 @@ namespace OUS
 {
     class WaveformAndRuler
     : public juce::Component
+    , public juce::FileDragAndDropTarget
     , private juce::ChangeListener
     {
     public:
@@ -15,16 +16,38 @@ namespace OUS
         WaveformAndRuler(juce::AudioFormatManager& formatManager);
         ~WaveformAndRuler() override;
         
-        // forwarding method
+        //==============================================================================
+        // TODO: find way to way to avoid this?
+        WaveformComponent const& getWaveform() const;
+        void addWaveformChangeListener(ChangeListener* listener);
+        void removeWaveformChangeListener(ChangeListener* listener);
+        
+        //==============================================================================
+        // forwarding methods
+        juce::Range<float> const& getVisibleRange() const;
+        juce::Range<float> const& getTotalRange() const;
+        
+        void setSampleRate(float sampleRate);
+        
+        void clear();
         void resetZoom();
+        
         std::function<void(juce::String&)> onNewFileDropped = nullptr;
         
         void setThumbnailSource(juce::AudioSampleBuffer* audioSource);
         
         //==============================================================================
+        // juce::Component
         void resized() override;
+        void mouseWheelMove(juce::MouseEvent const& event, juce::MouseWheelDetails const& wheel) override;
         
         //==============================================================================
+        // juce::FileDragAndDropTarget
+        bool isInterestedInFileDrag(const StringArray& files) override;
+        void filesDropped(const StringArray& files, int x, int y) override;
+        
+        //==============================================================================
+        // juce::ChangeListener
         void changeListenerCallback(juce::ChangeBroadcaster* source) override;
         
     private:
