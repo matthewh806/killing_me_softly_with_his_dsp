fdcab32eb26ef479bba1df4161bd5ddada2454d8
matthew <harris@ircam.fr>
2023-03-04 13:53:48 +0100

    CI/CD: Use static shared juce library for all

--- code/juce_shared_vst.cmake
+++ code/juce_shared_vst.cmake
@@ -1,14 +1,30 @@
-add_library(Shared_VST_Target INTERFACE)
+add_library(Shared_VST_Target STATIC)
 
-target_compile_definitions(Shared_VST_Target INTERFACE
-        # JUCE_WEB_BROWSER and JUCE_USE_CURL would be on by default, but you might not need them.
-        JUCE_WEB_BROWSER=0  # If you remove this, add `NEEDS_WEB_BROWSER TRUE` to the `juce_add_gui_app` call
-        JUCE_USE_CURL=0     # If you remove this, add `NEEDS_CURL TRUE` to the `juce_add_gui_app` call
-        JUCE_VST3_CAN_REPLACE_VST2=0)
+_juce_initialise_target(Shared_VST_Target)
 
-target_link_libraries(Shared_VST_Target INTERFACE
+target_link_libraries(Shared_VST_Target
+    PRIVATE
         juce::juce_audio_utils
+        juce::juce_dsp
         juce::juce_gui_extra
+    PUBLIC
         juce::juce_recommended_config_flags
-        juce::juce_recommended_lto_flags
         juce::juce_recommended_warning_flags)
+
+target_compile_definitions(Shared_VST_Target
+    PUBLIC
+        JUCE_WEB_BROWSER=0  # If you remove this, add `NEEDS_WEB_BROWSER TRUE` to the `juce_add_gui_app` call
+        JUCE_USE_CURL=0     # If you remove this, add `NEEDS_CURL TRUE` to the `juce_add_gui_app` callss
+    INTERFACE
+        $<TARGET_PROPERTY:Shared_VST_Target,COMPILE_DEFINITIONS>)
+
+target_include_directories(Shared_VST_Target
+    INTERFACE
+        $<TARGET_PROPERTY:Shared_VST_Target,INCLUDE_DIRECTORIES>)
+
+
+set_target_properties(Shared_VST_Target PROPERTIES
+        POSITION_INDEPENDENT_CODE TRUE
+        VISIBILITY_INLINES_HIDDEN TRUE
+        C_VISIBILITY_PRESET hidden
+        CXX_VISIBILITY_PRESET hidden)
